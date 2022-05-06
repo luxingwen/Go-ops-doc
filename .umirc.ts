b7a195bd71a814655e034616ae1c85add48a1816
Lewis <shuaicode@gmail.com>
2022-05-06 09:36:43 -0700

    fix

--- .umirc.ts
+++ .umirc.ts
@@ -7,5 +7,8 @@ export default defineConfig({
   logo: '/logo_github_icon_143196.png',
   outputPath: 'docs-dist',
   mode: 'site',
+  sitemap: {
+    hostname: "https://luxingwen.github.io/Go-ops-doc/"
+  }
   // more config: https://d.umijs.org/config
 });
